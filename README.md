96be748f449bc3bf21054422328cb8ac2396136b
Ben S. Thomas <fearlessleaderthomas@gmail.com>
2013-10-09 14:56:57 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SITS Helper
+
+This is an application to facilitate the management of Saganami Island Tactical Simulator. 
+
+by Ben Thomas
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SitsHelper::Application.config.secret_token = 'e3c60b007732a4d43d6e89f1665e3dbfda3e59c2b2d2a4db902e09a4bf4d367609f09d6a7fdb72f36061103aa9a58e24f7f9fa40c5719a938ae91aeae5326a62'
