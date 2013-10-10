d3edf103fb7f7e3a1b22bb55146afd3bcd852ee0
Ben S. Thomas <fearlessleaderthomas@gmail.com>
2013-10-10 10:14:56 -0400

    Fix User Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   # attr_accessible :title, :body
+
+  validates :name, presence: true
+  
 end
 
-validates :name, presence: true
